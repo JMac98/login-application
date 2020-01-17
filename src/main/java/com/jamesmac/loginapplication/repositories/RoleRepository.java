e724e9bacc85a7c04788e6bd14ed7e2126fce405
James Mac <32912083+JMac98@users.noreply.github.com>
2020-01-17 16:53:45 +0000

    Initial commit

---
+++ src/main/java/com/jamesmac/loginapplication/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.jamesmac.loginapplication.repositories;
+
+import com.jamesmac.loginapplication.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByRole(String role);
+
+}
---
+++ src/main/java/com/jamesmac/loginapplication/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jamesmac.loginapplication.repositories;
+
+
+import com.jamesmac.loginapplication.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
